e6b07d73643f158f5af9eb703daed7f4f938095c
MrDomine <d.d.dominus1989@gmail.com>
2022-03-30 17:44:34 +0200

    Añadida la dependencia de mysql

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.example.appvideoclub {
     requires javafx.fxml;
 
     requires org.kordamp.bootstrapfx.core;
+    requires java.sql;
 
     opens com.example.appvideoclub to javafx.fxml;
     exports com.example.appvideoclub;
