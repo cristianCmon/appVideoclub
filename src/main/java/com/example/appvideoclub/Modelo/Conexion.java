4603f4894cfb5a10371a41533a135177c486e425
MrDomine <d.d.dominus1989@gmail.com>
2022-03-30 17:43:09 +0200

    Creada Clase conexion a la BBDD

---
+++ src/main/java/com/example/appvideoclub/Modelo/Conexion.java
@@ -0,0 +1,36 @@
+package com.example.appvideoclub.Modelo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+    private String url;
+    private String bbdd;
+    private String usr;
+    private String pass;
+
+    private Connection cn;
+
+    public Conexion(String url, String bbdd, String usr, String pass) {
+        this.url = "jdbc:mysql://"+url+":3306/"+bbdd;
+        this.usr = usr;
+        this.pass = pass;
+    }
+
+    public Connection conectar(){
+        try {
+            if(cn==null) {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                cn = DriverManager.getConnection(url, usr, pass);
+            } else if(cn.isClosed()){
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                cn = DriverManager.getConnection(url, usr, pass);
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Error al conectar a la BBDD "+ bbdd);
+            return null;
+        }
+        return cn;
+    }
+}
